77e25403dd3f347e7ecb62ce3b9b4e79724b6d47
Luca Simone <info@lucasimone.info>
2022-03-07 15:47:25 +0100

    Clean up ad polished a bit the source code

--- stardust/stardust.py
+++ stardust/stardust.py
@@ -1,7 +1,7 @@
-import inspect
 import os
 from importlib.machinery import SourceFileLoader
 from inspect import getmembers
+from inspect import signature, isfunction, isbuiltin, iscoroutinefunction
 from typing import Any, Callable
 
 from starlette.applications import Starlette
@@ -21,60 +21,45 @@ class Stardust:
 
         try:
             with open(file_path) as f:
-                # exec(f.read(), {}, self.module)
-                # self.load_module(file_path)
-                # module_name = pathlib.Path(file_path)
-                # self.module = importlib.import_module(module_name)
-                # file_dir = os.path.dirname(file_path)
-                # sys.path.append(file_dir)
-                # exec(f.read(), globals(), self.module)
-
-                # spec = importlib.util.spec_from_file_location("app", file_path)
-                # self.module = importlib.util.module_from_spec(spec)
-                # spec.loader.exec_module(self.module)
-
                 self.module = SourceFileLoader("app", file_path).load_module()
 
         except FileNotFoundError:
             print("No such file or directory.")
             exit(1)
 
-        self.method = self._find_async_function(self.module, file_path)
+        self.method = self._find_local_function(self.module, file_path)
         if not self.method:
-            print("File must contain at least one async function.")
+            print("File must contain at least one local function.")
             exit(1)
 
     @staticmethod
-    def _find_async_function(module, file_path) -> Callable:
+    def _find_local_function(module, file_path) -> Callable:
         method = None
         # getmembers returns a list of tuples
         for member in getmembers(module):
             name, item = member
             if (
-                inspect.isfunction(item)
-                and not inspect.isbuiltin(item)
+                isfunction(item)
+                and not isbuiltin(item)
                 # we want to catch only local functions, not imported ones
                 and os.path.samefile(item.__code__.co_filename, file_path)
             ):
-                # if item.__module__ == module.__name__ and inspect.isfunction(item):
                 method = item
+
         return method
 
     async def __wrap_response(self, request: Request) -> Any:
+        call_if_params = (
+            lambda method, req: method(req)
+            if len(signature(method).parameters) > 0
+            else method()
+        )
 
-        # check if first params is type Request
-        if len(inspect.signature(self.method).parameters) > 0:
-            response = (
-                await self.method(request)
-                if inspect.iscoroutinefunction(self.method)
-                else self.method(request)
-            )
-        else:
-            response = (
-                await self.method()
-                if inspect.iscoroutinefunction(self.method)
-                else self.method()
-            )
+        response = (
+            await call_if_params(self.method, request)
+            if iscoroutinefunction(self.method)
+            else call_if_params(self.method, request)
+        )
 
         if not issubclass(response.__class__, Response):
             # by default, we reply with json...
